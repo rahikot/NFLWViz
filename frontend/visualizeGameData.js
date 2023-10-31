48467711fde73115802b69af6e18f202dc7ec4b3
Neil Patel <neilhpatel1@gmail.com>
2023-10-31 17:24:20 -0400

    Started work on reading input data and the UI

---
+++ frontend/visualizeGameData.js
@@ -0,0 +1,22 @@
+
+
+function displayTeams(textSvg, homeTeam, awayTeam) {
+    textSvg.append("text")
+      .attr("x", 20)
+      .attr("y", 25)
+      .text(homeTeam)
+      .attr("font-size", "32px")
+
+    textSvg.append("text")
+      .attr("x", 160)
+      .attr("y", 25)
+      .text("vs.")
+      .attr("font-size", "32px")
+
+    textSvg.append("text")
+      .attr("x", 300)
+      .attr("y", 25)
+      .text(awayTeam)
+      .attr("font-size", "32px")
+}
+
