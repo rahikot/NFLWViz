378cdcf3b36f6885bc9a72f3a17a5c6d14a86685
Neil Patel <neilhpatel1@gmail.com>
2023-11-02 13:55:02 -0400

    Added field image and ability to visualize a play

--- frontend/visualizeGameData.js
+++ frontend/visualizeGameData.js
@@ -1,5 +1,3 @@
-
-
 function displayTeams(textSvg, homeTeam, awayTeam) {
     textSvg.append("text")
       .attr("x", 20)
@@ -20,3 +18,63 @@ function displayTeams(textSvg, homeTeam, awayTeam) {
       .attr("font-size", "32px")
 }
 
+function scaleCoordinates(x, y) {
+    const xScale = d3.scaleLinear()
+        .domain([0, 120])
+        .range([81, 816])
+
+    const yScale = d3.scaleLinear()
+        .domain([0, 53.3])
+        .range([535, 155])
+
+    newX = xScale(x)
+    newY = yScale(y)
+
+    return [newX, newY]
+}
+
+function visualizePlay(allPlayData, playNumber, footballObject, interval) {
+    if (!allPlayData[playNumber]) {
+        throw new Error(`Play # '${playNumber}' could not be found.`)
+    }
+    playData = allPlayData[playNumber]
+
+    footballData = playData.filter(item => item.team === 'football')
+    //We can make modifications here to visualize all players
+
+    let index = 0;
+
+    const updateFootballLocation = () => {
+        if (index < playData.length) {
+            const footballData = playData.filter(item => item.team === 'football');
+            if (index < footballData.length) {
+                const data = footballData[index];
+                footballObject.attr("x", data.x).attr("y", data.y);
+                index++;
+            }
+        } else {
+            clearInterval(interval);
+        }
+    };
+
+    const intervalID = setInterval(updateFootballLocation, interval);
+
+}
+
+
+// Function that converts DateTime to seconds. Currently not used
+/*function scaleTimeData(data) {
+    var transformedData = {}
+    var firstDate = data[0].time
+
+    for (const rowNum in data) {
+        const row = data[rowNum];
+        const rowDate = row.time
+        const timeInSeconds = (rowDate - firstDate)/1000
+        transformedData[rowNum] = {
+            ...row,
+            time: timeInSeconds
+        }
+    }
+
+}*/
