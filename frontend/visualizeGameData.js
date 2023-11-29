5b85fa000f9487917607188b4aebe2180f707518
Neil Patel <neilhpatel1@gmail.com>
2023-11-28 20:51:25 -0500

    Added scoreboard and running game clock

--- frontend/visualizeGameData.js
+++ frontend/visualizeGameData.js
@@ -58,12 +58,142 @@ function visualizePlayers(playerData, color) {
 
     circles.attr("cx", function(d) { return d.x; })
         .attr("cy", function(d) { return d.y; });
-    
+
     circles.enter().append("circle")
         .attr("cx", function(d) { return d.x; })
         .attr("cy", function(d) { return d.y; })
         .attr("r", 5)
         .attr("fill", color);
+}
+
+function populateScoreBoard(mainSvg, awayColor, homeColor, homeTeamAbbr, awayTeamAbbr) {
+    const homeTeam = mainSvg.select("#homeTeam");
+    const awayTeam = mainSvg.select("#awayTeam");
+    const gameInfo = mainSvg.select("#gameInfo");
+
+    homeTeam.style("display", "block")
+    awayTeam.style("display", "block")
+    gameInfo.style("display", "block")
+
+    homeTeam.selectAll("text").remove();
+    awayTeam.selectAll("text").remove();
+    gameInfo.selectAll("text").remove();
+
+    awayTeam.select("rect").attr("fill", awayColor)
+    homeTeam.select("rect").attr("fill", homeColor)
+
+    homeTeam.append("text")
+      .attr("x", 5)
+      .attr("y", 35)
+      .text(homeTeamAbbr)
+      .attr("class", "text-styling")
+
+    homeTeam.append("text")
+      .attr("id", "homeScore")
+      .attr("y", 35)
+      .text("0")
+      .attr("text-anchor", "end")
+      .each(function () {
+        const textElement = d3.select(this);
+        const homeTeamWidth = homeTeam.node().getBBox().width;
+        textElement.attr("x", homeTeamWidth - 18)
+          .attr("class", "text-styling")
+      });
+
+    xCoordinate = 135;
+
+    if (awayTeamAbbr && awayTeamAbbr.length === 2) {
+        xCoordinate += 15;
+    }
+
+    awayTeam.append("text")
+      .attr("x", xCoordinate)
+      .attr("y", 35)
+      .text(awayTeamAbbr)
+      .attr("class", "text-styling")
+
+    awayTeam.append("text")
+      .attr("id", "awayScore")
+      .attr("x", 5)
+      .attr("y", 35)
+      .text("0")
+      .attr("class", "text-styling")
+
+    homeTeam.select("rect").attr("fill", homeColor)
+    awayTeam.select("rect").attr("fill", awayColor)
+
+    gameInfo.append("text")
+      .attr("id", "Quarter")
+      .attr("x", 5)
+      .attr("y", 30)
+      .text("1st")
+      .attr("class", "text-styling")
+
+    gameInfo.append("text")
+      .attr("id", "GameClock")
+      .attr("x", gameInfo.node().getBBox().width / 2 - 10)
+      .attr("y", 30)
+      .text("15:00")
+      .attr("text-anchor", "middle")
+      .attr("class", "text-styling")
+
+    gameInfo.append("text")
+      .attr("id", "Down")
+      .attr("x", 265)
+      .attr("y", 30)
+      .text("1st Down")
+      .attr("class", "text-styling")
+
+    mainSvg.selectAll("circle").remove();
+
+}
+
+function updateScoreBoard(mainSvg, playData) {
+    const homeTeam = mainSvg.select("#homeTeam");
+    const awayTeam = mainSvg.select("#awayTeam");
+    const gameInfo = mainSvg.select("#gameInfo");
+
+    const ordinalMap = {
+        "1": "1st",
+        "2": "2nd",
+        "3": "3rd",
+        "4": "4th"
+    };
+
+    ordinalQuarter = ordinalMap[playData.quarter]
+    ordinalDown = ordinalMap[playData.down] + " Down"
+
+    gameInfo.select("#Quarter").text(ordinalQuarter)
+    gameInfo.select("#GameClock").text(playData.gameClock)
+    gameInfo.select("#Down").text(ordinalDown)
+
+    homeTeam.select("#homeScore").text(playData.preSnapHomeScore)
+    awayTeam.select("#awayScore").text(playData.preSnapVisitorScore)
+}
+
+function decrementClock(mainSvg) {
+  const gameInfo = mainSvg.select("#gameInfo");
+  const gameClock = gameInfo.select("#GameClock");
+  const currentTime = gameClock.text();
+  const [minutes, seconds] = currentTime.split(":").map(Number);
+
+  const totalSeconds = minutes * 60 + seconds;
+  const newTotalSeconds = totalSeconds - 1;
+  const finalTotalSeconds = Math.max(newTotalSeconds, 0);
+
+  const newMinutes = Math.floor(finalTotalSeconds / 60);
+  const newSeconds = finalTotalSeconds % 60;
+  const newTimeString = `${padZero(newMinutes)}:${padZero(newSeconds)}`;
+  gameClock.text(newTimeString);
+}
+
+
+function padZero(number) {
+  return number < 10 ? `0${number}` : number;
+}
+
+function getTeamColor(teamAbbr) {
+  return getComputedStyle(document.documentElement).getPropertyValue(`--team-color-${teamAbbr}`) || "blue";
 }
 
 function visualizePlay(allPlayData, playNumber, mainSvg, interval, homeTeam, awayTeam) {
@@ -93,6 +223,10 @@ function visualizePlay(allPlayData, playNumber, mainSvg, interval, homeTeam, awa
     var keys = Object.keys(timeToData)
     let index = 0;
 
+    const homeColor = getTeamColor(homeTeamAbbr);
+    const awayColor = getTeamColor(awayTeamAbbr);
+
+
     const updateLocation = () => {
 
         if (index < keys.length) {
@@ -105,10 +239,14 @@ function visualizePlay(allPlayData, playNumber, mainSvg, interval, homeTeam, awa
                 throw new Error(`For play # '${playNumber}', multiple (x, y) coordinates for football
                 found at datapoint '${currTimeData}'`)
             }
-            visualizePlayers(homeData, "red")
-            visualizePlayers(awayData, "blue")
+            visualizePlayers(homeData, homeColor);
+            visualizePlayers(awayData, awayColor);
             mainSvg.select(".football").attr("x", footballData[0].x).attr("y", footballData[0].y);
             index++;
+
+            if (index % 10 === 0) {
+              decrementClock(mainSvg);
+            }
 
         } else {
             clearInterval(interval);
@@ -116,6 +254,7 @@ function visualizePlay(allPlayData, playNumber, mainSvg, interval, homeTeam, awa
     };
 
     const intervalID = setInterval(updateLocation, interval);
-     mainSvg.selectAll("circle").remove();
+    mainSvg.selectAll("circle").remove();
 
 }
+
